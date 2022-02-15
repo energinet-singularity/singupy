b95cab4dfb45dce6b8a3986990ae6c278d67268d
Anders Wittendorff <andersw89@gmail.com>
2022-02-15 14:24:47 +0000

    Corrected folder structure and updated setup.py.
    Previoes folder structure was incorrect, and "tests" folder was not excluded from libary setup.

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,9 +1,6 @@
-import pytest
-import os
-from kafkahelper import hellosingu 
+from kafkahelper import hellosingu
 
-# Check all files files can be parsed
+
 def test_hello_user():
     obj = hellosingu.HelloSingu(user_initials="ABC")
-    pass
     assert obj.say_hey() == "Well hello there ABC!"
