ac73c07af885fa7f00905b31fb57668e89e249af
Anders Wittendorff <andersw89@gmail.com>
2022-02-16 09:40:41 +0000

    Removed json from requirements in setup.py.
    It is part og python.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Anders Wittendorff',
     author_email='andersw89@gmail.com',
     packages=find_packages(exclude=['tests']),
-    install_requires=['json', 'kafka'],
+    install_requires=['kafka-python'],
     version='0.0',
     license='Apache License 2.0',
     description='Libary for Singularity',
