a16fe2cd61fdd7f6819982ddf7543a657c161ce0
Karsten Rasmussen <bolsmand@gmail.com>
2022-03-25 13:03:41 +0000

    WIP

--- singupy/api.py
+++ singupy/api.py
@@ -1,10 +1,20 @@
+# Generic
+from modulefinder import Module
+import re
+import os
+import logging
+from threading import Thread
+from typing import Union, List
+
+# For DataFrameAPI
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
-import re
-import logging
-from threading import Thread
 from pandasql import sqldf
+
+# For FileLoader
+import smbclient
+
 
 # Initialize log
 log = logging.getLogger(__name__)
@@ -14,7 +24,7 @@ class DataFrameAPI():
     def __init__(self, data: pd.DataFrame, host: str = '0.0.0.0', port: int = 80,
                  query_regex: str = r'^SELECT [^;]*;$', dataname: str = 'dataframe'):
         # Load dataframe and setup flask api environment
-        self.update_data(data, dataname)
+        self.update_data(data)
         self.__app = Flask(__name__)
         self.__api = Api(self.__app)
 
@@ -30,7 +40,7 @@ class DataFrameAPI():
         # Starts the flask app
         self.__app.run(host=host, port=port)
 
-    def __get_df(self):
+    def __get_df(self) -> pd.DataFrame:
         # Used to send the dataframe to the __querydata subclass
         return self.data
 
@@ -80,7 +90,7 @@ class DataFrameAPI():
                 try:
                     return sqldf(args['sql-query'], globals()).to_dict(), 200
                 except Exception as e:
-                    return {'error': f"Query failed with message '{e}'", 'example': "pd-query=first_name=='Evan'"}, 401
+                    return {'error': f"Query failed with message '{e}'", 'example': "sql-query=SELECT * FROM sqldata WHERE first_name='Evan';"}, 401
 
             elif args['pd-query'] is not None:
                 log.debug(f"PD Query: {args['pd-query']}")
@@ -91,3 +101,49 @@ class DataFrameAPI():
 
             else:
                 return {'error': "No query specified - either use 'sql-query' or 'pd-query'."}, 401
+
+class FileLoader():
+    __source: dict = {"path": None, "os": None}
+    _source_path: str = None
+    _destination_path: str = None
+    max_age_s: int = None
+    file_regex_list: List[str] = None
+
+    def __init__(self, input_path: str, output_path: str, max_age_s: int = None, file_regex: Union[str, List[str]] = '.*'):
+        self.source_path = input_path
+        self.output_path = output_path
+        self.max_age_s = max_age_s
+        if isinstance(file_regex, list):
+            self.file_regex_list = file_regex
+        else:
+            self.file_regex_list = [file_regex]
+        
+    def __get_os_module(self, path: str) -> Module:
+        if path.startswith('\\\\') or path.startswith('//'):
+            return smbclient._os
+        else:
+            return os
+
+    @property
+    def source_path(self):
+        return self.__source['path']
+
+    @source_path.setter
+    def source_path(self, path: str):
+        self.__source['path'] = path
+        self.__source['os'] = self.__get_path_module()
+
+    @property
+    def destination_path(self):
+        return self._destination_path
+
+    @destination_path.setter
+    def destination_path(self, path: str):
+        self._destination_path = path
+        self._destination_client = self.__get_path_module()
+
+    def transfer_files(self):
+        smbclient._os.scandir()
+        os.scandir
+    
+
