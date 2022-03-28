b38ce1704574be1f932bf046a6bb088fac3021d4
Karsten Rasmussen <bolsmand@gmail.com>
2022-03-28 09:03:02 +0000

    temp save

--- singupy/api.py
+++ singupy/api.py
@@ -5,6 +5,7 @@ import os
 import logging
 from threading import Thread
 from typing import Union, List
+from datetime import datetime, timedelta, timezone
 
 # For DataFrameAPI
 from flask import Flask
@@ -12,8 +13,9 @@ from flask_restful import Resource, Api, reqparse
 import pandas as pd
 from pandasql import sqldf
 
-# For FileLoader
-import smbclient
+# For FileManager
+import smbclient as smb_src
+import smbclient as smb_dst
 
 
 # Initialize log
@@ -102,48 +104,117 @@ class DataFrameAPI():
             else:
                 return {'error': "No query specified - either use 'sql-query' or 'pd-query'."}, 401
 
-class FileLoader():
-    __source: dict = {"path": None, "os": None}
-    _source_path: str = None
-    _destination_path: str = None
-    max_age_s: int = None
-    file_regex_list: List[str] = None
+class FileManager():
+    __src: dict = {
+        'path': None,
+        'remote': False,
+        'func': {},
+        'credentials': {'user': None, 'pass': None}
+    }
+    __dst: dict = {
+        'path': None,
+        'remote': False,
+        'files': [],
+        'func': {},
+        'credentials': {'user': None, 'pass': None}
+    }
 
-    def __init__(self, input_path: str, output_path: str, max_age_s: int = None, file_regex: Union[str, List[str]] = '.*'):
-        self.source_path = input_path
-        self.output_path = output_path
+    def __init__(self, source: str, destination: str, max_age_s: int = None, file_regex: Union[str, List[str]] = '.*',
+                 smb_username: str = None, smb_password: str = None):
+        self.source = source
+        self.destination = destination
         self.max_age_s = max_age_s
         if isinstance(file_regex, list):
             self.file_regex_list = file_regex
         else:
             self.file_regex_list = [file_regex]
-        
-    def __get_os_module(self, path: str) -> Module:
-        if path.startswith('\\\\') or path.startswith('//'):
-            return smbclient._os
+        self.set_credentials(smb_username, smb_password)
+
+    def __attach_credentials(self):
+        for config in [self.__src, self.__dst]:
+            if config['remote']:
+                cred = config['credentials']
+                config['func']['client'].ClientConfig(username=cred['user'], password=cred['pass'])
+
+    def __update_config_func(self, source: bool, isremote: bool):
+        if source:
+            func = self.__src['func']
+            smb = smb_src
         else:
-            return os
+            func = self.__dst['func']
+            smb = smb_dst
+
+        if isremote:
+            func['client'] = smb
+            func['remove'] = smb.remove
+            func['scandir'] = smb.scandir
+        else:
+            func['client'] = None
+            func['remove'] = os.remove
+            func['scandir'] = os.scandir
+
+    def __update_config(self, path: str, source: bool):
+        if source:
+            config = self.__src
+        else:
+            config = self.__dst
+            config['files'] = []
+
+        config['path'] = path
+        if path.startswith('\\\\') or path.startswith('//'):
+            config['remote'] = True
+        else:
+            config['remote'] = False
+
+        self.__update_config_func(source, config['remote'])
+        self.__attach_credentials()
 
     @property
-    def source_path(self):
-        return self.__source['path']
+    def source(self):
+        return self.__src['path']
 
-    @source_path.setter
-    def source_path(self, path: str):
-        self.__source['path'] = path
-        self.__source['os'] = self.__get_path_module()
+    @source.setter
+    def source(self, path: str):
+        self.__update_config(path, True)
 
     @property
-    def destination_path(self):
-        return self._destination_path
+    def destination(self):
+        return self.__dst['path']
 
-    @destination_path.setter
-    def destination_path(self, path: str):
-        self._destination_path = path
-        self._destination_client = self.__get_path_module()
+    @destination.setter
+    def destination(self, path: str):
+        self.__update_config(path, False)
+
+    def set_credentials(self, username: str = None, password: str = None, for_source: bool = True, for_destination: bool = True):
+        cred = {'user': username, 'pass': password}
+
+        if for_source:
+            self.__src['credentials'] = cred
+        if for_destination:
+            self.__dst['credentials'] = cred
+        
+        self.__attach_credentials()
+    
+    def killme(self):
+        print(smb_src.ClientConfig.username)
+        print(smb_dst.ClientConfig.username)
 
     def transfer_files(self):
-        smbclient._os.scandir()
-        os.scandir
-    
-
+        filelist = self.__src['func']['scandir'](self.__src['path'])
+        for file in [{'name': file.name, 'mtime': file.stat().st_mtime} for file in filelist if file.is_file()]:
+            # Check if the file (with modify timestamp) is known
+            if file not in self.__dst['files']:
+                # Check if the file is older than max age - skip it if so
+                if self.max_age_s is not None:
+                    file_age_s = (datetime.utcnow() - datetime.utcfromtimestamp(file['mtime'])).total_seconds()
+                    if file_age_s > self.max_age_s:
+                        log.debug(f"Ignoring file '{file['name']}' because it is '{file_age_s}' seconds old (limit is '{self.max_age_s}')")
+                        self.__dst['files'].append(file)
+                        continue
+                
+                # Remove any old file-reference
+                self.__dst['files'] = [d_file for d_file in self.__dst['files'] if d_file['name'] != file['name']]
+                
+                # move file
+                self.__dst['files'].append(file)
+            
